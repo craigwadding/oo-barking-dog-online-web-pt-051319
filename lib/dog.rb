121592ee07ff7f5a87a84a8f58c5265d74e9f706
craig wadding <craig.wadding@gmail.com>
2019-05-30 02:27:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,17 @@
 class Dog 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name 
+  end 
+  
+  def name 
+    @this_dogs_name 
+  end 
+  
   def bark 
     puts "Woof!"
   end 
 end 
+
+
 
     # Your code goes here!
